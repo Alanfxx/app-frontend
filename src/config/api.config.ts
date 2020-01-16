9dc2aa1e990999daee0ff2fa93fe5738eebbe84b
Alanfxx <j.alands2@gmail.com>
2020-01-16 10:34:26 -0300

    Testando app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://alanfx-api-ionic.herokuapp.com",
     bucketBaseUrl: "https://alanfx-spring-ionic.s3-sa-east-1.amazonaws.com"
 }
